ba77598f23a660166641287d4ef6fe7736fe9d55
Ehabw57 <maestro.ehab123@gmail.com>
2025-10-09 16:15:27 +0300

    Add navbar component

---
+++ src/components/navbar/navbar.ts
@@ -0,0 +1,17 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  navLinks: { name: string; link: string }[] = [
+    { name: 'Home', link: '/' },
+    { name: 'Tasks', link: '/' },
+    { name: 'Settings', link: '/settings' },
+  ];
+}
